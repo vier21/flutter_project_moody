1431e76ba9d1968dbe42eac86e3178bb551f1b8e
andhikaptr <andhikapasagi55@gmail.com>
2023-04-07 20:44:40 +0700

    Login page melakukan perubahan

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:flutter_tubes_satu/StoryPage.dart';
 import 'package:flutter_tubes_satu/main.dart';
 
 void main() {
